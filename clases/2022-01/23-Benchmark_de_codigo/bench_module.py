52f2c257e4964831cce1011570b04f542aba385d
Pablo Badilla <pbadilla.torrealba@gmail.com>
2022-06-13 21:10:15 -0400

    Clase 25 - JIT, paralelismo y computación distribuida

--- clases/2022-01/23-Benchmark_de_codigo/bench_module.py
+++ clases/2022-01/23-Benchmark_de_codigo/bench_module.py
@@ -1,12 +1,11 @@
 
 
 def benchmark_sum(n):
-    """Funcion de referencia que suma n elementos transformados
-    """
+    """Funcion de referencia que suma n elementos transformados"""
     ac = []
 
     for i in range(n):
-        to_sum = [(i // 2) ** n + (i - n) ** (n // 3) for i in range(n)]
+        to_sum = [(j // 2) ** n + (j - n) ** (n // 3) for j in range(n)]
         sum_ = sum(to_sum)
         ac.append(sum_)
 
